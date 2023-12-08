fa39285cf2cac3ee69343caf714d61772c711a53
Daksh <akshd@outlook.com>
2023-12-08 16:46:01 +0530

    chore: extract wallet ui

---
+++ src/components/Wallet.Component.tsx
@@ -0,0 +1,123 @@
+import React, { useState } from 'react'
+import { TWalletDetails } from "../types";
+import styles from "../pages/index.module.css";
+import { useForm } from 'react-hook-form';
+import { createAccount } from '@turnkey/viem';
+import { TurnkeyClient } from '@turnkey/http';
+import { createWalletClient, http } from 'viem';
+import { sepolia } from 'viem/chains';
+
+type signingFormData = {
+    messageToSign: string;
+};
+
+
+type TSignedMessage = {
+    message: string;
+    signature: string;
+} | null;
+
+
+
+function Wallet({ wallet, passkeyHttpClient }: { wallet: TWalletDetails, passkeyHttpClient: TurnkeyClient }) {
+    const { register: signingFormRegister, handleSubmit: signingFormSubmit } =
+        useForm<signingFormData>();
+
+    const [signedMessage, setSignedMessage] = useState<TSignedMessage>(null);
+
+    const signMessage = async (data: signingFormData) => {
+        if (!wallet) {
+            throw new Error("wallet not found");
+        }
+
+        const viemAccount = await createAccount({
+            client: passkeyHttpClient,
+            organizationId: wallet.subOrgId,
+            signWith: wallet.address,
+            ethereumAddress: wallet.address,
+        });
+
+        const viemClient = createWalletClient({
+            account: viemAccount,
+            chain: sepolia,
+            transport: http(),
+        });
+
+        const signedMessage = await viemClient.signMessage({
+            message: data.messageToSign,
+        });
+
+        setSignedMessage({
+            message: data.messageToSign,
+            signature: signedMessage,
+        });
+    };
+
+    return (
+        <div>
+            <div className={styles.info}>
+                ETH address: <br />
+                <span className={styles.code}>{wallet.address}</span>
+            </div>
+            <div>
+                <h2>Now let&apos;s sign something!</h2>
+                <p className={styles.explainer}>
+                    We&apos;ll use a{" "}
+                    <a
+                        href="https://viem.sh/docs/accounts/custom.html"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Viem custom account
+                    </a>{" "}
+                    to do this, using{" "}
+                    <a
+                        href="https://www.npmjs.com/package/@turnkey/viem"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        @turnkey/viem
+                    </a>
+                    . You can kill your NextJS server if you want, everything happens on
+                    the client-side!
+                </p>
+                <form
+                    className={styles.form}
+                    onSubmit={signingFormSubmit(signMessage)}
+                >
+                    <input
+                        className={styles.input}
+                        {...signingFormRegister("messageToSign")}
+                        placeholder="Write something to sign..."
+                    />
+                    <input
+                        className={styles.button}
+                        type="submit"
+                        value="Sign Message"
+                    />
+                </form>
+                {signedMessage && (
+                    <div className={styles.info}>
+                        Message: <br />
+                        <span className={styles.code}>{signedMessage.message}</span>
+                        <br />
+                        <br />
+                        Signature: <br />
+                        <span className={styles.code}>{signedMessage.signature}</span>
+                        <br />
+                        <br />
+                        <a
+                            href="https://etherscan.io/verifiedSignatures"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Verify with Etherscan
+                        </a>
+                    </div>
+                )}
+            </div>
+        </div>
+    )
+}
+
+export default Wallet
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,34 +1,18 @@
 import Image from "next/image";
 import styles from "./index.module.css";
 import { TurnkeyClient } from "@turnkey/http";
-import { createAccount } from "@turnkey/viem";
 import { useForm } from "react-hook-form";
 import { WebauthnStamper } from "@turnkey/webauthn-stamper";
 import { useState } from "react";
-import { createWalletClient, http } from "viem";
-import { sepolia } from "viem/chains";
 import { TWalletDetails } from "../types";
 import Create from "@/components/Create.Section";
-
-
-type signingFormData = {
-  messageToSign: string;
-};
-
+import Wallet from "@/components/Wallet.Component";
 
 type TWalletState = TWalletDetails | null;
 
-type TSignedMessage = {
-  message: string;
-  signature: string;
-} | null;
-
-
 export default function Home() {
   const [wallet, setWallet] = useState<TWalletState>(null);
-  const [signedMessage, setSignedMessage] = useState<TSignedMessage>(null);
-  const { register: signingFormRegister, handleSubmit: signingFormSubmit } =
-    useForm<signingFormData>();
+
   const { register: _loginFormRegister, handleSubmit: loginFormSubmit } =
     useForm();
 
@@ -43,34 +27,6 @@ export default function Home() {
     stamper
   );
 
-  const signMessage = async (data: signingFormData) => {
-    if (!wallet) {
-      throw new Error("wallet not found");
-    }
-
-    const viemAccount = await createAccount({
-      client: passkeyHttpClient,
-      organizationId: wallet.subOrgId,
-      signWith: wallet.address,
-      ethereumAddress: wallet.address,
-    });
-
-    const viemClient = createWalletClient({
-      account: viemAccount,
-      chain: sepolia,
-      transport: http(),
-    });
-
-    const signedMessage = await viemClient.signMessage({
-      message: data.messageToSign,
-    });
-
-    setSignedMessage({
-      message: data.messageToSign,
-      signature: signedMessage,
-    });
-  };
-
   return (
     <main className={styles.main}>
       <a href="https://turnkey.com" target="_blank" rel="noopener noreferrer">
@@ -84,80 +40,13 @@ export default function Home() {
         />
       </a>
       <div>
-        {wallet !== null && (
-          <div className={styles.info}>
-            Your sub-org ID: <br />
-            <span className={styles.code}>{wallet.subOrgId}</span>
-          </div>
-        )}
         {wallet && (
-          <div className={styles.info}>
-            ETH address: <br />
-            <span className={styles.code}>{wallet.address}</span>
-          </div>
+          <Wallet wallet={wallet} passkeyHttpClient={passkeyHttpClient} />
         )}
-        {signedMessage && (
-          <div className={styles.info}>
-            Message: <br />
-            <span className={styles.code}>{signedMessage.message}</span>
-            <br />
-            <br />
-            Signature: <br />
-            <span className={styles.code}>{signedMessage.signature}</span>
-            <br />
-            <br />
-            <a
-              href="https://etherscan.io/verifiedSignatures"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Verify with Etherscan
-            </a>
-          </div>
-        )}
+
       </div>
       {!wallet && (
         <Create passkeyHttpClient={passkeyHttpClient} setWallet={setWallet} />
-      )}
-      {wallet !== null && (
-        <div>
-          <h2>Now let&apos;s sign something!</h2>
-          <p className={styles.explainer}>
-            We&apos;ll use a{" "}
-            <a
-              href="https://viem.sh/docs/accounts/custom.html"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Viem custom account
-            </a>{" "}
-            to do this, using{" "}
-            <a
-              href="https://www.npmjs.com/package/@turnkey/viem"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              @turnkey/viem
-            </a>
-            . You can kill your NextJS server if you want, everything happens on
-            the client-side!
-          </p>
-          <form
-            className={styles.form}
-            onSubmit={signingFormSubmit(signMessage)}
-          >
-            <input
-              className={styles.input}
-              {...signingFormRegister("messageToSign")}
-              placeholder="Write something to sign..."
-            />
-            <input
-              className={styles.button}
-              type="submit"
-              value="Sign Message"
-            />
-          </form>
-        </div>
       )}
     </main>
   );
