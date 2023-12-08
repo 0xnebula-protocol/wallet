3db523cbfe63cd3145ce9ec5070a90009fe5c2e9
Daksh <akshd@outlook.com>
2023-12-08 16:48:32 +0530

    chore: change copy

--- src/components/Wallet.Component.tsx
+++ src/components/Wallet.Component.tsx
@@ -62,24 +62,7 @@ function Wallet({ wallet, passkeyHttpClient }: { wallet: TWalletDetails, passkey
             <div>
                 <h2>Now let&apos;s sign something!</h2>
                 <p className={styles.explainer}>
-                    We&apos;ll use a{" "}
-                    <a
-                        href="https://viem.sh/docs/accounts/custom.html"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        Viem custom account
-                    </a>{" "}
-                    to do this, using{" "}
-                    <a
-                        href="https://www.npmjs.com/package/@turnkey/viem"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        @turnkey/viem
-                    </a>
-                    . You can kill your NextJS server if you want, everything happens on
-                    the client-side!
+                    Sign a message using your passkeys wallet.
                 </p>
                 <form
                     className={styles.form}
